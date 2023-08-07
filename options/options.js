09646afab71efd3a3735b320bb238ca0afa8869b
Crowdbotics <team@crowdbotics.com>
2023-08-07 20:50:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-cloud-42712",
+  url: "https://shiny-cloud-42712.botics.co",
+  api: "https://shiny-cloud-42712.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
